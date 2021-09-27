8f7f6e9000a6b260ea2b8612c97ccf23070f07c8
Roman Kovalevskiy <kovalevskyi.roma@gmail.com>
2021-09-27 13:54:52 +0300

    DeserializingJsonPage76

---
+++ DeserializingJsonPage76.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+
+namespace DeserializingJsonPage76
+{
+    public class Product
+    {
+        public string ProductId { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\QaLearning\\jsonfile.json";
+            string filecontent = File.ReadAllText(path);
+            Product product = JsonConvert.DeserializeObject<Product>(filecontent);
+            Console.WriteLine(product.ProductId);
+            Console.WriteLine(product.Description);
+            Console.WriteLine(product.Price);
+        }
+    }
+}
