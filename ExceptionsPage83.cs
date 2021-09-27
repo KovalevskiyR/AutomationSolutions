1e73dfb7a74ce7d61b42bcc0edf2be0c9f7f5a9c
Roman Kovalevskiy <kovalevskyi.roma@gmail.com>
2021-09-27 13:55:30 +0300

    DeserializingJsonPage76

---
+++ ExceptionsPage83.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace ExceptionsPage83
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StreamReader sr = new StreamReader("C:\\QaLearning\\UserData.txt");
+                string line = sr.ReadToEnd();
+                Console.WriteLine(line);
+            }
+
+            catch
+            {
+                Console.WriteLine("Please create file!");
+            }
+
+            finally
+            {
+                Console.WriteLine("Operation is finished");
+            }
+        }
+    }
+}
